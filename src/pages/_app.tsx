f51bae44a542d9cc8e20dc0fbee7085d6496a55b
yan <yanhaibiao1991@gmail.com>
2025-04-22 15:33:12 +0800

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
+import "@tomo-inc/tomo-web-sdk/style.css";
 import type { AppProps } from "next/app";
+import { TomoContextProvider } from "@tomo-inc/tomo-web-sdk";
+
+
+import '@mysten/dapp-kit/dist/index.css';
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <TomoContextProvider
+    theme="light"
+    chainTypes={["sui"]}
+    clientId="fSjHVdz7gaQr12E8Qs8Ng5Mge3xpZTvfpEIDS4CwXLX2BAKlQ2zTe726hoyfD80XK8C1jecwtaXJfbQ1REYTlKlk"
+  >
+    <Component {...pageProps} />
+  </TomoContextProvider>
+  );
 }
